c3de43e06f4ea11eadf67aa1c49e2419fa31315f
Tharshika-E-1 <tharshikae1@gmail.com>
2026-07-13 09:44:44 +0530

    Fix Vercel server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-import express from "express"
-import "dotenv/config"
-import cors from "cors"
-import connectDB from "./configs/db.js"
-import userRouter from "./routes/userRoutes.js"
-import chatRouter from "./routes/chatRoutes.js"
-import messageRouter from "./routes/messageRoutes.js"
-import creditRouter from "./routes/creditRoutes.js"
-import { stripeWebhooks } from "./controllers/webhooks.js"
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+import connectDB from "./configs/db.js";
+
+import userRouter from "./routes/userRoutes.js";
+import chatRouter from "./routes/chatRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import creditRouter from "./routes/creditRoutes.js";
+import { stripeWebhooks } from "./controllers/webhooks.js";
 import websiteRouter from "./routes/websiteRoutes.js";
 import shareRouter from "./routes/shareRoute.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
@@ -14,9 +16,10 @@ import communityRouter from "./routes/communityRoutes.js";
 import ragRouter from "./routes/ragRoutes.js";
 import visionRouter from "./routes/visionRoutes.js";
 
-const app = express()
+const app = express();
 
-await connectDB()
+await connectDB();
+
 // Stripe Webhooks
 app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
@@ -25,11 +28,12 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => res.send("Server is Live!"))
-app.use('/api/user', userRouter)
-app.use('/api/chat', chatRouter)
-app.use('/api/message', messageRouter)
-app.use('/api/credit', creditRouter)
+app.get("/", (req, res) => res.send("Server is Live!"));
+
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
+app.use("/api/credit", creditRouter);
 app.use("/api/website", websiteRouter);
 app.use("/api/share", shareRouter);
 app.use("/api/dashboard", dashboardRoutes);
@@ -37,8 +41,4 @@ app.use("/api/community", communityRouter);
 app.use("/api/rag", ragRouter);
 app.use("/api/vision", visionRouter);
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+export default app;
